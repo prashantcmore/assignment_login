6cee697cf986114d89da4ed98131f3fac31940e1
prashant <prashantmore8698@gmail.com>
2022-07-07 22:53:43 +0530

    resolving bugs

--- lib/screens/authentification.dart
+++ lib/screens/authentification.dart
@@ -132,109 +132,84 @@ class _AuthCardState extends State<AuthCard> {
     } catch (error) {
       const errorMessage = 'Could not authenticate you. Please try again later.';
       _showErrorDialog(errorMessage);
-    }
 
-    setState(() {
-      _isLoading = false;
-    });
-  }
-
-  void _switchAuthMode() {
-    if (_authMode == AuthMode.SignIn) {
       setState(() {
-        _authMode = AuthMode.SignUp;
-      });
-    } else {
-      setState(() {
-        _authMode = AuthMode.SignIn;
+        _isLoading = false;
       });
     }
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Container(
-              child: Column(
-                //   mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(top: 8),
-                    child: Text(
-                      "Welcome Back",
-                      style: TextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(top: 5, bottom: 20),
-                    child: Text(
-                      "Sign Up to continue",
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 18,
+    void _switchAuthMode() {
+      if (_authMode == AuthMode.SignIn) {
+        setState(() {
+          _authMode = AuthMode.SignUp;
+        });
+      } else {
+        setState(() {
+          _authMode = AuthMode.SignIn;
+        });
+      }
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Column(
+        children: [
+          Container(
+            child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Container(
+                child: Column(
+                  //   mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(top: 8),
+                      child: Text(
+                        "Welcome Back",
+                        style: TextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: MediaQuery.of(context).size.height * 0.045,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[400],
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: TextFormField(
-                        decoration:
-                            const InputDecoration(hintText: 'E-Mail or Phone', prefixIcon: Icon(Icons.email_outlined)),
-                        keyboardType: TextInputType.emailAddress,
-                        onSaved: (value) {
-                          _authData['email'] = value!;
-                        },
+                    const Padding(
+                      padding: EdgeInsets.only(top: 5, bottom: 20),
+                      child: Text(
+                        "Sign Up to continue",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 18,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: MediaQuery.of(context).size.height * 0.045,
-                decoration: BoxDecoration(
-                  color: Colors.grey[400],
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Password',
-                    fillColor: Colors.blue,
-                    prefixIcon: Icon(Icons.lock_outline),
-                    suffixIcon: IconButton(
-                      onPressed: null,
-                      icon: Icon(Icons.remove_red_eye_outlined),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: MediaQuery.of(context).size.height * 0.045,
+                        decoration: BoxDecoration(
+                          color: Colors.grey[400],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                              hintText: 'E-Mail or Phone', prefixIcon: Icon(Icons.email_outlined)),
+                          keyboardType: TextInputType.emailAddress,
+                          onSaved: (value) {
+                            _authData['email'] = value!;
+                          },
+                        ),
+                      ),
                     ),
-                  ),
-                  obscureText: true,
-                  controller: _passwordController,
-                  onSaved: (value) {
-                    _authData['password'] = value!;
-                  },
+                  ],
                 ),
               ),
-            ),
-            if (_authMode == AuthMode.SignUp)
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Container(
                   height: MediaQuery.of(context).size.height * 0.045,
-                  decoration: BoxDecoration(color: Colors.grey[400], borderRadius: BorderRadius.circular(10)),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[400],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                   child: TextFormField(
-                    enabled: _authMode == AuthMode.SignUp,
                     decoration: const InputDecoration(
-                      hintText: 'Confirm Password',
+                      hintText: 'Password',
+                      fillColor: Colors.blue,
                       prefixIcon: Icon(Icons.lock_outline),
                       suffixIcon: IconButton(
                         onPressed: null,
@@ -242,55 +217,86 @@ class _AuthCardState extends State<AuthCard> {
                       ),
                     ),
                     obscureText: true,
+                    controller: _passwordController,
                     onSaved: (value) {
                       _authData['password'] = value!;
                     },
                   ),
                 ),
               ),
-            const Padding(
-              padding: EdgeInsets.all(8),
-              child: Text(
-                "Forgot Password ?",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.black),
-              ),
-            ),
-          ]),
-        ),
-        Column(
-          children: [
-            RaisedButton(
-              child: Text(_authMode == AuthMode.SignIn ? 'Sign In' : 'SIGN UP'),
-              onPressed: () => _submit(),
-              // _submit(),
-              //  Navigator.of(context).pushNamed(ProductsOverviewScreen.routeName),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
-              color: Colors.blue[200],
-              textColor: Colors.blue,
-            ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.002),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Don't have account ?",
-                  style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
-                ),
-                FlatButton(
-                    child: Text(
-                      ' ${_authMode == AuthMode.SignIn ? 'SIGNUP' : 'SIGNIN'} ',
-                      style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
+              if (_authMode == AuthMode.SignUp)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: MediaQuery.of(context).size.height * 0.045,
+                    decoration: BoxDecoration(color: Colors.grey[400], borderRadius: BorderRadius.circular(10)),
+                    child: TextFormField(
+                      enabled: _authMode == AuthMode.SignUp,
+                      decoration: const InputDecoration(
+                        hintText: 'Confirm Password',
+                        prefixIcon: Icon(Icons.lock_outline),
+                        suffixIcon: IconButton(
+                          onPressed: null,
+                          icon: Icon(Icons.remove_red_eye_outlined),
+                        ),
+                      ),
+                      obscureText: true,
+                      onSaved: (value) {
+                        _authData['password'] = value!;
+                      },
                     ),
-                    onPressed: _switchAuthMode,
-                    textColor: Colors.black),
-              ],
-            ),
-          ],
-        ),
-      ],
-    );
+                  ),
+                ),
+              const Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  "Forgot Password ?",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.black),
+                ),
+              ),
+            ]),
+          ),
+          Column(
+            children: [
+              RaisedButton(
+                child: Text(_authMode == AuthMode.SignIn ? 'Sign In' : 'SIGN UP'),
+                onPressed: () => _submit(),
+                // _submit(),
+                //  Navigator.of(context).pushNamed(ProductsOverviewScreen.routeName),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+                color: Colors.blue[200],
+                textColor: Colors.blue,
+              ),
+              SizedBox(height: MediaQuery.of(context).size.height * 0.002),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't have account ?",
+                    style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+                  ),
+                  FlatButton(
+                      child: Text(
+                        ' ${_authMode == AuthMode.SignIn ? 'SIGNUP' : 'SIGNIN'} ',
+                        style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
+                      ),
+                      onPressed: _switchAuthMode,
+                      textColor: Colors.black),
+                ],
+              ),
+            ],
+          ),
+        ],
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
