e28dff27904658534bbd1b3034cd312727a6d2d7
prashant <prashantmore8698@gmail.com>
2022-07-07 22:37:19 +0530

    second commit

--- lib/screens/authentification.dart
+++ lib/screens/authentification.dart
@@ -122,7 +122,7 @@ class _AuthCardState extends State<AuthCard> {
       } else if (error.toString().contains('INVALID_EMAIL')) {
         errorMessage = 'This is not a valid email address';
       } else if (error.toString().contains('WEAK_PASSWORD')) {
-        errorMessage = 'This password is too weak.';
+        errorMessage = 'This password is too weak .';
       } else if (error.toString().contains('EMAIL_NOT_FOUND')) {
         errorMessage = 'Could not find a user with that email.';
       } else if (error.toString().contains('INVALID_PASSWORD')) {
